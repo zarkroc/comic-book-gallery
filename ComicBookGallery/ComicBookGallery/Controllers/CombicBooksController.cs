411649ce8497cb43aa5c9d5a4067fc09d2bda7d4
zarkroc <9654120+zarkroc@users.noreply.github.com>
2018-08-13 15:47:50 +0200

    Added a view

---
+++ ComicBookGallery/ComicBookGallery/Controllers/CombicBooksController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class CombicBooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
